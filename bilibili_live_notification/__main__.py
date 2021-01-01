11614b7055978edaae8e16b5ebbc3b4a842e6ef2
NateScarlet <NateScarlet@Gmail.com>
2021-01-02 00:05:04 +0800

    chore(desp): update bilibili-api to >=3.1.6

--- bilibili_live_notification/__main__.py
+++ bilibili_live_notification/__main__.py
@@ -1,15 +1,10 @@
 """Send email notification when bilibili live start. """
-import asyncio
 import logging
 from datetime import datetime, timedelta
 
 from bilibili_api import live
 
 from . import config, emailtools
-
-
-async def _debug(event):
-    logging.debug(event)
 
 
 def _format_time(v: datetime) -> str:
@@ -44,21 +39,6 @@ def iterate_rooms():
         yield room
 
 
-# https://github.com/Passkou/bilibili_api/pull/91
-def connect_all_LiveDanmaku(*livedanmaku_classes):
-    """
-    同时连接多个直播间
-    :param livedanmaku_classes: LiveDanmaku类动态参数
-    :return:
-    """
-    tasks = []
-    for room in livedanmaku_classes:
-        task = room.connect(True)
-        tasks.append(task)
-
-    asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
-
-
 if __name__ == '__main__':
     logging.root.setLevel(logging.INFO)
     if config.TEST_EMAIL_TO:
@@ -68,4 +48,4 @@ if __name__ == '__main__':
             f'[启动] - {_format_time(datetime.now())}',
             '服务启动测试邮件',
         )
-    connect_all_LiveDanmaku(*iterate_rooms())
+    live.connect_all_LiveDanmaku(*iterate_rooms())
